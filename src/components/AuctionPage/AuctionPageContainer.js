6701cb47bd45528aa1c68d7bd03b43bca0ac1333
Martin Cissel <mcissel@gmail.com>
2019-03-12 18:18:35 -0400

    routes set up

---
+++ src/components/AuctionPage/AuctionPageContainer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+class AuctionPageContainer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+  render() {
+    const {} = this.props
+
+    return (
+      <div className="auction">
+        howdy this is an auction
+      </div>
+    )
+  }
+}
+
+export default AuctionPageContainer
---
+++ src/components/Home/HomeContainer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+  render() {
+    const {} = this.props
+    return (
+      <div className="home">
+        This is the home page
+      </div>
+    )
+  }
+}
+
+export default Home
