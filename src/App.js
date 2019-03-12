dcc14aa0a799f12d06aa4ee4839ca77df96cfa34
Martin Cissel <mcissel@gmail.com>
2019-03-12 18:22:29 -0400

    adding a redux store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux'
 import { Router, Route, Switch } from 'react-router-dom'
-
 import { createBrowserHistory } from 'history'
+
+import rootReducer from './redux/rootReducer'
 
 import Home from './components/Home';
 import AuctionPage from './components/AuctionPage';
 import './App.css';
 
 const history = createBrowserHistory();
+const store = createStore(rootReducer);
 
 class App extends Component {
   render() {
     return (
-      <Router history={history}>
-        <Switch>
-          <Route path="/auction/:id" exact strict={false} component={AuctionPage} />
-          <Route component={Home} />
-        </Switch>
-      </Router>
+
+      <Provider store={store}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auction/:id" exact strict={false} component={AuctionPage} />
+            <Route component={Home} />
+          </Switch>
+        </Router>
+      </Provider>
     );
   }
 }
