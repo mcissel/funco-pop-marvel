7cc4478d49417d02a80ede0cffc0528422fb3029
Martin Cissel <mcissel@gmail.com>
2019-03-12 21:12:59 -0400

    moving data fetch into HOC

--- src/components/Home/HomeContainer.js
+++ src/components/Home/HomeContainer.js
@@ -1,45 +1,23 @@
 import React from 'react'
-import fetchJsonp from 'fetch-jsonp'
+import { compose } from 'redux'
 import { connect } from 'react-redux'
 
+import withAuctionData from './withAuctionData'
 import {setAuctions, setFeaturedAuction} from '../../redux/auction/auctionActions';
 import {sortedByPriceHighest, sortedByPriceLowest, sortedAlphabetical} from '../../redux/auction/auctionSelectors';
 import HomeView from './HomeView'
-import {parseResponse} from './helpers'
 
-const appId = process.env.REACT_APP_EBAY_APP_ID;
-const keywords = ['funco', 'pop', 'marvel'];
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props)
-    console.log(`%c ${appId}`, 'background:#bada55; color:#222;')
-  }
-
-  componentDidMount() {
-    let requestUrl = 'https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords';
-    requestUrl += '&SERVICE-VERSION=1.0.0';
-    requestUrl += `&SECURITY-APPNAME=${appId}`;
-    requestUrl += '&RESPONSE-DATA-FORMAT=JSON';
-    requestUrl += '&REST-PAYLOAD';
-    requestUrl += `&keywords=${keywords.join('%20')}`;
-
-    fetchJsonp(requestUrl)
-      .then((response) => {
-        return response.json()
-      }).then((json) => {
-        const items = parseResponse(json);
-        console.log(`%citems`, 'background:#bada55; color:#222;', items)
-        window.ti = items;
-        this.props.dispatch(setAuctions(items))
-        this.props.dispatch(setFeaturedAuction(items[0].itemId))
-      }).catch(function(error) {
-        console.log('parsing failed', error)
-      })
-  }
-
   componentDidUpdate(prevProps, prevState) {
+    const { auctions, featuredAuctionId, dispatch } = this.props
+    if (auctions !== prevProps.auctions) {
+      dispatch(setAuctions(auctions))
+    }
 
+    if (featuredAuctionId !== prevProps.featuredAuctionId) {
+      dispatch(setFeaturedAuction(featuredAuctionId))
+    }
   }
 
   render() {
@@ -60,4 +38,7 @@ const mapStateToProps = state => ({
   }
 })
 
-export default connect(mapStateToProps)(Home)
+export default compose(
+  withAuctionData,
+  connect(mapStateToProps),
+)(Home)
---
+++ src/components/Home/withAuctionData.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import fetchJsonp from "fetch-jsonp";
+import {parseResponse} from "./helpers";
+
+const appId = process.env.REACT_APP_EBAY_APP_ID;
+const keywords = ['funco', 'pop', 'marvel'];
+
+const withAuctionData = WrappedComponent => {
+  class AuctionProvider extends React.Component {
+    state = {
+      auctions: null,
+      featuredAuctionId: null,
+    }
+
+    componentDidMount() {
+      let requestUrl = 'https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords';
+      requestUrl += '&SERVICE-VERSION=1.0.0';
+      requestUrl += `&SECURITY-APPNAME=${appId}`;
+      requestUrl += '&RESPONSE-DATA-FORMAT=JSON';
+      requestUrl += '&REST-PAYLOAD';
+      requestUrl += `&keywords=${keywords.join('%20')}`;
+
+      fetchJsonp(requestUrl)
+        .then((response) => {
+          return response.json()
+        }).then((json) => {
+          const items = parseResponse(json);
+          console.log(`%citems`, 'background:#bada55; color:#222;', items)
+          window.ti = items;
+          this.setState({
+            auctions: items,
+            featuredAuctionId: items[0].itemId,
+          })
+        }).catch(function(error) {
+          console.log('parsing failed', error)
+        })
+    }
+
+    render() {
+      const { auctions, featuredAuctionId } = this.state
+
+      return (
+        <WrappedComponent
+          auctions={auctions}
+          featuredAuctionId={featuredAuctionId}
+        />
+      )
+    }
+  }
+
+  return AuctionProvider
+}
+
+export default withAuctionData;
